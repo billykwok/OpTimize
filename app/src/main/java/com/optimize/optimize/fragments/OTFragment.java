bae5ade1989a598bc184755c0b2ba343ded2631e
samwalker <samwalker505@gmail.com>
2015-01-06 02:08:18 +0800

    added data_structure.txt

--- app/src/main/java/com/optimize/optimize/fragments/OTFragment.java
+++ app/src/main/java/com/optimize/optimize/fragments/OTFragment.java
@@ -21,7 +21,6 @@ public class OTFragment extends Fragment {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
